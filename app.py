220ece3052e9e5d6668aaab1a97988f962137ba4
Sivaji Chintha <chinthasivaji123@gmail.com>
2026-03-16 21:57:41 +0530

    Updated UI: Centered form and added CSS styling

--- app.py
+++ app.py
@@ -36,5 +36,5 @@ def predict():
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
